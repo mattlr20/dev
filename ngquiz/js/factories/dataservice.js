876e8cbf86236c9ab225e070ef007791cbfe7d2c
Matthew Richardson <mattlr20@gmail.com>
2017-02-16 14:13:15 -0500

    Update dataservice.js

--- ngquiz/js/factories/dataservice.js
+++ ngquiz/js/factories/dataservice.js
@@ -7,12 +7,15 @@
     function DataFactory(){
         var dataObj = {
             turtlesData: turtlesData,
-            quizQuestions: quizQuestions
+            quizQuestions: quizQuestions,
+            correctAnswers: correctAnswers
         };
 
         return dataObj;
     }
 
+    var correctAnswers = [1, 2, 3, 0, 2, 0, 3, 2, 0, 3];
+    
     var quizQuestions  = [
         {
             type: "text",
