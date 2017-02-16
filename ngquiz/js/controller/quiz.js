03c156828c4a0f2cad3e308f0b30bb21bd12dcc2
Matthew Richardson <mattlr20@gmail.com>
2017-02-16 14:11:39 -0500

    Update quiz.js

--- ngquiz/js/controller/quiz.js
+++ ngquiz/js/controller/quiz.js
@@ -14,21 +14,34 @@
             vm.dataService = DataService;
             vm.questionAnswered = questionAnswered;
             vm.setActiveQuestion = setActiveQuestion;
+            vm.selectAnswer = selectAnswer;
+            vm.finaliseAnswers = finaliseAnswers;
             vm.activeQuestion = 0;
+            vm.error = false;
+            vm.finalise = false;
 
             var numQuestionsAnswered = 0;
 
-            function setActiveQuestion(){
-                var breakOut = false;
-                var quizLength = DataService.quizQuestions.length - 1;
+            function setActiveQuestion(index){
+                if(index === undefined){
+                    var breakOut = false;
+                    var quizLength = DataService.quizQuestions.length - 1;
 
-                while(!breakOut){
-                    vm.activeQuestion = vm.activeQuestion < quizLength?++vm.activeQuestion:0;
+                    while(!breakOut){
+                        vm.activeQuestion = vm.activeQuestion < quizLength?++vm.activeQuestion:0;
 
-                    if(DataService.quizQuestions[vm.activeQuestion].selected === null){
-                        breakOut = true; //go back to find unanswered question
+                        if(vm.activeQuestion === 0) {
+                            vm.error = true;
+                        }
+
+                        if(DataService.quizQuestions[vm.activeQuestion].selected === null){
+                            breakOut = true; //go back to find unanswered question
+                        }
                     }
+                } else {
+                    vm.activeQuestion = index;
                 }
+                    
             }
 
             function questionAnswered() {
@@ -39,12 +52,33 @@
                     numQuestionsAnswered++;
                     if(numQuestionsAnswered >= quizLength){
                         //finalise quiz
+                        for(var i = 0; i < quizLength; i++){
+                            if(DataService.quizQuestions[i].selected === null) {
+                                setActiveQuestion(i);
+                                return;
+                            }
+                        }
+                        vm.error = false;
+                        vm.finalise = true;
+                        return;
                     }
                 }
 
                 vm.setActiveQuestion();
             }
 
+            function selectAnswer(index) {
+                DataService.quizQuestions[vm.activeQuestion].selected = index;
+            }
+
+            function finaliseAnswers() {
+                vm.finalise = false;
+                numQuestionsAnswered = 0;
+                vm.activeQuestion = 0;
+                quizMetrics.markQuiz();
+                quizMetrics.changeState("quiz", false);
+                quizMetrics.changeState("results", true);
+            }
         }
 
 
